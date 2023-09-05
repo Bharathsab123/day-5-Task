eb4bbbfd53a2467614b37d146930e2fea4beadfa
Bharath S <129163112+Bharathsab123@users.noreply.github.com>
2023-09-05 17:42:08 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,48 @@
+let MyResume={
+    "Profile":{
+        "Name":"Bharath",
+        "email": "bharathsab123@gmail.com",
+        "pnoneNo":6381218113,
+        "Degree":"B.E"
+    },
+    "Location" :{
+        "Address":"3/897 maniyammai streer,palavakkam,chennai-41",
+        "city": "chennai",
+        "state": "tamilnadu",
+        "country":"india",
+    },
+    "Education":[{
+        "10std":{
+           'schoolName':"Syed Ammal Higer Secondary school",
+           "yearOfPassing":2017,
+           "percentage": 85.5
+        },
+        "12th std":{
+            'schoolName':"Syed Ammal Higer Secondary school",
+            "yearOfPassing":2019,
+            "percentage": 60.1
+        },
+        "undergraduate":{
+            "collegename":"MNM jain engineering college",
+            "branch":"B.E mechanical",
+            "yearOfPassing":2023,
+            "percentage": 79.8
+        }
+    }],
+    "skills":{
+        "programming language":`python,SQL,javascript 
+        Basic in ms office`,
+        "level":"basic"
+    },
+    "personal information":[{
+        'date of birth':"29/08/2001",
+        "languages":[{"language":"tamil,English"}],
+        "github id":"https://github.com/Bharathsab123",
+        "Professional Id":"//www.Linkedin.Com/In/Bharath-S-198a94234"
+
+        
+    }]
+    
+    
+}
+console.log(MyResume);
